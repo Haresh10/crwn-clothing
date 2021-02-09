b4dd5b4d4dc48fcfadd3014023294257d35e0f53
Apple <apple@Apples-MacBook-Pro.local>
2021-02-09 11:14:42 +0530

    updated cart utils to group items in cart reducer and added quantity field

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,8 +1,12 @@
 import { findAllByTitle } from "@testing-library/react";
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cartAction";
 import "./collectionitem.scss";
+import CustomButton from "../custom-button/CustomButton";
 
-const CollectionItem = ({ name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, imageUrl, price } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +14,13 @@ const CollectionItem = ({ name, imageUrl, price }) => {
         <span className="title">{name}</span>
         <span className="price">${price}</span>
       </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add To Cart
+      </CustomButton>
     </div>
   );
 };
-
-export default CollectionItem;
+const mapDispatchToProps = (dipatch) => ({
+  addItem: (item) => dipatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cartAction.js
+++ src/redux/cart/cartAction.js
@@ -2,3 +2,7 @@ import { CartActionTypes } from "./cart.action.types";
 export const toggleCartDropdown = () => ({
   type: CartActionTypes.TOGGLE_CART_DROPDOWN,
 });
+export const addItem = (cartItem) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: cartItem,
+});
